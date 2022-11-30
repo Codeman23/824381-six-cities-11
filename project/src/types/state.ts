5cdb7d8657ed3d5b4a9a858b0c987749af3e8a11
Anton Dubovskiy <a.n.dubovskiy@gmail.com>
2022-11-30 16:14:36 +0300

    history-route and react-toastify added

--- project/src/types/state.ts
+++ project/src/types/state.ts
@@ -1,7 +1,22 @@
 import { store } from '../store/index.js';
+import { Offer } from './offer.js';
+import { AuthorizationStatus } from '../const';
 
 type State = ReturnType<typeof store.getState>;
 
 type AppDispatch = typeof store.dispatch;
 
-export { type State, type AppDispatch };
+type UserProcess = {
+  authorizationStatus: AuthorizationStatus;
+};
+
+type DataProcess = {
+  offers: Offer[];
+  isOffersLoaded: boolean;
+};
+
+type AppActionData = {
+  city: string;
+};
+
+export { type State, type AppDispatch, type UserProcess, type DataProcess, type AppActionData };
